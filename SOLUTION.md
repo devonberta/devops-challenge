52b6943117229cbff79cc84ac8151cd18497765a
Devon Berta <devonbertamisc@gmail.com>
2024-10-10 16:56:33 -0700

    adding final solution that has been tested for challenge 3. Works in both localstack and actual aws. Localstack gave false impression of success with RDS module. Took some additional troubleshooting to complete

--- SOLUTION.md
+++ SOLUTION.md
@@ -30,27 +30,35 @@ Removed local docker images and forced a pull from the registry. Container ran a
 ## Challenge-2 comments:
 
 * Tested with docker destkop kubernetes instead of kind, minikube, or k3s
-* built a terraform module for the helm chart also to manage deployment and include it here
+* Need to add local service entry to local systems hosts file to handle for local testing. This would change in a real world deployment on a non local cluster. /etc hosts entry: 127.0.0.1       kubernetes.docker.internal challenge2.host.local
+* deployment commands:
+    * helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+    * helm repo update
+    * helm install my-ingress-nginx ingress-nginx/ingress-nginx
+    * cd {to challenge 2 directory}
+    * helm install devonberta-devops-challenge ./devonberta-devops-challenge
+    * curl http://challege2.host.local/
 
 ## challenge-3 comments:
 
-* Two example solutions provided: 
-    * one is reusing aws maintianed modules and putting them into a deployment terraform which seems to be what was the expected approach
-    * two is I made seperate modules form scratch for the resources that implemented a simplified model for deployment
-* the reason of providing two approaches is to highlight that sometimes the existing modules introduce unnecessary complexity in managing of resources that can be solved with a purpose built solution
 * based on the limited information in the diagram I gathered the desired resoruces where:
     * 1 vpc
     * 1 internet gateway
     * 1 load balancers configured for multi az across 3 azs
-    * 3 nat instances
-    * 1 autoscale group across 3 az's 
+    * 3 nat gateways
+    * 1 autoscale group across 3 availability zones 
+    * 3 ec2 instances deployed by the autoscale group across Availability zones
     * 3 private subnets spread across availablity zones
     * 1 nsg
-    * 1 multi AZ RDS DB Cluster spread across 3 azs
+    * 1 multi AZ RDS DB Cluster spread across 3 availability zones
     * 1 S3 bucket
 * running the terraform I have not hard coded the region in the provider cofig. It is typical when run through ci/cd tools this value may need to be changed depending on destination deploying too. This can be set via environment variables instead along with credentials. 
-* the alternative is to configure a provider for all the possible regions and use alias's when calling modules but did not implement that in this example
 * commands:
     * AWS_ACCESS_KEY_ID="" AWS_SECRET_ACCESS_KEY="" AWS_REGION="us-east-1" terraform init
     * AWS_ACCESS_KEY_ID="" AWS_SECRET_ACCESS_KEY="" AWS_REGION="us-east-1" terraform plan -var 'db_password=my_password!'
     * AWS_ACCESS_KEY_ID="" AWS_SECRET_ACCESS_KEY="" AWS_REGION="us-east-1" terraform apply -var 'db_password=my_password!'
+* tflocal commands:
+    * AWS_REGION="us-east-1" tflocal init
+    * AWS_REGION="us-east-1" tflocal plan -var 'db_password=my_password!'
+    * AWS_REGION="us-east-1" tflocal apply -var 'db_password=my_password!'
+* You can test with localstack by commenting out the data source in data.tf and commenting out line 86, finally uncomment line 85 before running. That will force the default ami to be used instead of the broken ami lookup feature in localstack. 
--- challenge-3/main.tf
+++ challenge-3/main.tf
@@ -15,6 +15,51 @@ module "vpc" {
   one_nat_gateway_per_az = true
 }
 
+// Create ALB and security group rules
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.11.0"
+  vpc_id = module.vpc.vpc_id
+  name               = "${var.deployment.name-prefix}-alb"
+  load_balancer_type = "application"
+  subnets            = module.vpc.public_subnets
+  enable_deletion_protection = false
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+}
+resource "aws_lb_target_group" "asg" {
+  name_prefix = "asg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_alb_listener" "application_listener" {
+  load_balancer_arn = module.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.asg.arn
+    type             = "forward"
+  }
+}
+
+// Create Autoscale group and launch template to setup apache2 in ubuntu image
 resource "aws_security_group" "instance_sg" {
   name        = "${var.deployment.name-prefix}-instance-sg"
   description = "Security group for EC2 instances"
@@ -35,61 +80,9 @@ resource "aws_security_group" "instance_sg" {
   }
 }
 
-resource "aws_security_group" "rds_sg" {
-  name        = "${var.deployment.name-prefix}-rds-sg"
-  description = "Allow traffic for RDS"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "9.11.0"
-  vpc_id = module.vpc.vpc_id
-  name               = "${var.deployment.name-prefix}-alb"
-  load_balancer_type = "application"
-  subnets            = module.vpc.public_subnets
-  security_groups    = [aws_security_group.alb_sg.id]
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
-    }
-  }
-  target_groups = [
-    {
-      name-prefix = "${var.deployment.name-prefix}"
-      port        = 80
-      protocol    = "HTTP"
-      target_type = "instance"
-    }
-  ]
-}
-
 resource "aws_launch_template" "apache2" {
   name          = "${var.deployment.name-prefix}-apache2-config"
+  //image_id      = "ami-df5de72bdb3b"
   image_id      = data.aws_ami.ubuntu-image.id
   instance_type = "t2.micro"
   user_data = base64encode(file("${path.module}/user-data.sh"))
@@ -106,7 +99,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size                  = var.deployment.max-instances
   min_size                  = var.deployment.min-instances
   vpc_zone_identifier = module.vpc.private_subnets
-
+  target_group_arns         = [aws_lb_target_group.asg.arn]
   launch_template {
     id      = aws_launch_template.apache2.id
     version = "$Latest"
@@ -114,6 +107,7 @@ resource "aws_autoscaling_group" "asg" {
 
 }
 
+// Create RDS Subnet, Security Group, and Deployment
 resource "aws_db_subnet_group" "subnet_group" {
   name       = "${var.deployment.name-prefix}-subnet-group"
   subnet_ids = module.vpc.private_subnets
@@ -127,17 +121,34 @@ module "rds" {
   source  = "terraform-aws-modules/rds-aurora/aws"
   version = "~> 9.9.1"
   apply_immediately      = true
-  database_name          = "${var.deployment.name-prefix}-db"
+  database_name          = "${var.deployment.name-prefix}db"
   db_subnet_group_name   = aws_db_subnet_group.subnet_group.name
   engine                 = "aurora-mysql"
   engine_version         = "5.7"
   instance_class         = "db.t2.small"
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  //vpc_security_group_ids = [aws_security_group.rds_sg.id]
   master_username        = "admin"
   master_password        = var.db_password
   name                   = lower(var.deployment.name-prefix)
+  vpc_id = module.vpc.vpc_id
+  skip_final_snapshot   = true
+  security_group_rules  = {
+    ingress = {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      cidr_blocks = ["10.0.0.0/16"]
+    }
+    egress = {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 }
 
+// Create S3 Bucket
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.deployment.name-prefix}-bucket"
   tags = {
